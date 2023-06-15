ad38b44ebe8158f078fa49d1c8a8db213c472664
Shreyas Mourya <shreyasmourya5@gmail.com>
2023-06-16 00:47:30 +0530

    player customization work

--- Assets/PlayerCustomization.cs
+++ Assets/PlayerCustomization.cs
@@ -18,6 +18,9 @@ public class PlayerCustomization : MonoBehaviour
     
     public List<Character> Characters = new List<Character>();
 
+    public List<GameObject> MaleCustoms = new List<GameObject>();
+    public List<GameObject> FemaleCustoms = new List<GameObject>();
+
     public int GenderIndex;
     public int MainBodyIndex;
     public int HairsIndex;
@@ -26,21 +29,54 @@ public class PlayerCustomization : MonoBehaviour
     public int BeardIndex;
     public int VestIndex;
 
+    private void Awake()
+    {
+        ChangeGender();
+    }
     public void SelectGender(int index)
     {
         GenderIndex = index;
+        ChangeGender();
     }
-  
+    public void ChangeGender()
+    {
+        if (GenderIndex == 0)
+        {
+            foreach (GameObject body in MaleCustoms) //   <--- go back to here --------+
+            {
+                // do work
+                body.SetActive(true);
+            }
+            foreach (GameObject body in FemaleCustoms) //   <--- go back to here --------+
+            {
+                // do work
+                body.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (GameObject body in MaleCustoms) //   <--- go back to here --------+
+            {
+                // do work
+                body.SetActive(false);
+            }
+            foreach (GameObject body in FemaleCustoms) //   <--- go back to here --------+
+            {
+                // do work
+                body.SetActive(true);
+            }
+        }
+
+    }
     public void SelectMainBody(int index)
     {
-
-        if (MainBodyIndex < Characters[GenderIndex].MainBody.Count)
+       
+        if (MainBodyIndex < Characters[GenderIndex].MainBody.Count - 1)
         {
             MainBodyIndex += index;
 
             if (MainBodyIndex == -1)
-                MainBodyIndex = Characters[GenderIndex].MainBody.Count;
-           
+                MainBodyIndex = Characters[GenderIndex].MainBody.Count - 1;     
         }         
         else
         {
@@ -48,6 +84,26 @@ public class PlayerCustomization : MonoBehaviour
                 MainBodyIndex = 0;
             else
                 MainBodyIndex += index;
+        }
+        foreach (GameObject body in Characters[GenderIndex].MainBody) //   <--- go back to here --------+
+        {                        
+            if (body == Characters[GenderIndex].MainBody[MainBodyIndex])                                  
+            {
+                Characters[GenderIndex].MainBody[MainBodyIndex].SetActive(true);
+
+                if(GenderIndex == 0)
+                    MaleCustoms.Add(Characters[GenderIndex].MainBody[MainBodyIndex]);
+                else
+                    FemaleCustoms.Add(Characters[GenderIndex].MainBody[MainBodyIndex]);
+                continue;   // Skip the remainder of this iteration. -----+
+            }
+
+            if (GenderIndex == 0)
+                MaleCustoms.Remove(body);
+            else
+                FemaleCustoms.Remove(body);
+            // do work
+            body.SetActive(false);
         }
     }
 }
